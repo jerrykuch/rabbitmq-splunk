703dda4a1a08335618084ec4709becfce17d6338
Jerry Kuch <jerrykuch@gmail.com>
2012-02-09 09:06:04 -0800

    Modify wrapper script to pass args more cleanly.

--- amqp_ping_check.py
+++ amqp_ping_check.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python
 import sys, os, datetime
-
-# Hack to load splunk packages
-#from optparse import OptionParser
-#parser = OptionParser()
-#(options, args) = parser.parse_args()
-#_SPLUNK_PYTHON_PATH = args[0]
-#sys.path.append(_SPLUNK_PYTHON_PATH)
-
-#import splunk
-#import cherrypy
-
 import pika
 
-#server, port = sys.argv[1].split(":")
-server = "ubuntu"
-port = "55672"
-vhost = "/"
-username = "guest"
-password = "guest"
-#vhost = sys.argv[2]
-#username = sys.argv[3]
-#password = sys.argv[4]
+# BUGBUG:  Need usage message and option parsing
+#  amqp_ping_check.py server:port vhost username password
+server, port = sys.argv[1].split(":")
+vhost = sys.argv[2]
+username = sys.argv[3]
+password = sys.argv[4]
 
 creds_broker = pika.PlainCredentials(username, password)
 conn_params = pika.ConnectionParameters(server,
@@ -32,14 +18,14 @@ try:
     conn_broker = pika.BlockingConnection(conn_params)
     channel = conn_broker.channel()
 except Exception:
-    print "%s ERROR %s could not connect to %s:%s!" % (str(now), 
-                                                       __file__, 
-                                                       server, 
+    print "%s ERROR %s could not connect to %s:%s!" % (str(now),
+                                                       __file__,
+                                                       server,
                                                        port)
     exit(1)
 
 print "%s OK %s connection to %s:%s successful." % (str(now),
-                                                    __file__, 
-                                                    server, 
+                                                    __file__,
+                                                    server,
                                                     port)
 exit(0)
--- wrapper.py
+++ wrapper.py
@@ -1,16 +1,27 @@
 import os
+import sys
 import subprocess
 
+# Capture where your system Python interpreter is (not Splunk's embedded one);
+# This lets us add packages to a Python environment that isn't the embedded
+# Splunk one, thereby letting them survive Splunk updates, not interfere with
+# other Splunk-provided packages, etc.
 _NEW_PYTHON_PATH = '/usr/bin/python'
 _SPLUNK_PYTHON_PATH = os.environ['PYTHONPATH']
-
 os.environ['PYTHONPATH'] = _NEW_PYTHON_PATH
-os.unsetenv('LD_LIBRARY_PATH')
+os.unsetenv('LD_LIBRARY_PATH')   # Unset to stifle annoying log warnings
 
-#my_process = os.path.join(os.getcwd(), 'amqp_ping_check.py')
-my_process = os.path.join('/opt/splunk/bin/scripts', 'amqp_ping_check.py')
+script_name  = sys.argv[1]
+surplus_args = sys.argv[2:]
 
-p = subprocess.Popen([os.environ['PYTHONPATH'], my_process, _SPLUNK_PYTHON_PATH], 
-                      stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+script_path = os.path.join('/opt/splunk/bin/scripts', script_name)
+popen_args  = [os.environ['PYTHONPATH'], script_path] + surplus_args
+
+p = subprocess.Popen(popen_args,
+                     stdin=subprocess.PIPE,
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.STDOUT)
+
 output = p.communicate()[0]
+
 print output
