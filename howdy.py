51c7207a0429d4d99bd618ba79a190d0e1218b2f
Jerry Kuch <jerrykuch@gmail.com>
2012-02-09 09:19:25 -0800

    Get rid of toy placeholder scripts.

--- howdy.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import datetime
-
-now = datetime.datetime.now()
-print str(now) + " HOWDY!"
-
--- howdy2.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import sys, datetime
-
-now = datetime.datetime.now()
-print str(now) + " HOWDY, " + sys.argv[1]
-
