43c0e6eb0af739319712ef9f7adcde5e774b25bc
Jerry Kuch <jerrykuch@gmail.com>
2012-02-29 16:41:36 -0800

    Make queue integrity check work, needs slight tidying.

--- queue_integrity_check.py
+++ queue_integrity_check.py
@@ -12,6 +12,8 @@ vhost = sys.argv[2]
 username = sys.argv[3]
 password = sys.argv[4]
 queue_name = sys.argv[5]
+is_durable = json.loads(sys.argv[6].lower())
+is_auto_delete = json.loads(sys.argv[7].lower())
 
 conn = httplib.HTTPConnection(server, port)
 path = "/api/queues/%s/%s" % (urllib.quote(vhost, safe=""),
@@ -29,18 +31,23 @@ try:
 except socket.error:
     print "ERROR Could not connect to %s:%s" % (server, port)
     exit(1)
+
 response = conn.getresponse()
 
 # Check salient error codes...
 if response.status == 404:
     print "ERROR queue %s does not exist." % (queue_name)
     exit(1)
-elif
-    response.status > 299:
-    print "ERROR unexpected API error %s checking queue %s" % (response.read(),
+elif response.status > 299:
+    print "ERROR unexpected API error %s checking queue %s" % (response.status,
                                                                queue_name)
+    exit(1)
+
+print "response.status = %s" % response.status
+#print ">>>>\n%s\n" % response.read()
 
 response = json.loads(response.read())
+
 if response["auto_delete"] != is_auto_delete:
     print "ERROR queue %s auto_delete flag is not %s as expected." % (queue_name,
                                                                       is_auto_delete)
